202408ba675bb206b60f9f5bfc7910801fe82d93
Alexandrbolshakov <alexandrbolshakov@yahoo.com>
2020-09-21 08:41:55 +0300

    add animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
-console.log('Hello!');
+import './lib/lib';
+import $ from './lib/lib';
+
+// $('button').on('click', function(){
+//     $('div').eq(2).toggleClass('active');
+// });
+
+// console.log($('div').eq(2).find('.more'));
+// console.log($('.more').eq(0).siblings());
+
+// $('.findme').fadeIn(1800);
